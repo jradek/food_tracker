ef347ba05e230cca8d15de5c7aa7c01841eeaf37
Jaromir Radek <jradek@yandex.com>
2019-05-12 19:52:44 +0200

    refactoring- better typing- more functional

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-import textwrap
-from collections import namedtuple
+import typing
 
-Ingredients = namedtuple("Ingredients", ["fat", "carb", "prot"])
 
-Product = namedtuple(
-    "Product",
-    [
-        "id",
-        #'name',
-        #'desc',
-        "kcal",
-        "ingredients",
-        #'kJ'
-    ],
-)
+class Ingredients(typing.NamedTuple):
+    fat: float  # in grams
+    carb: float  # in grams
+    prot: float  # in grams
 
-Serving = namedtuple("Serving", ["product_id", "factor", "macros", "kcal"])
+
+class Product(typing.NamedTuple):
+    """
+    """
+
+    id: str
+    # name: str
+    # desc: str
+    kcal: float  # energy as provided by manufacturer
+    ingredients: Ingredients  # ingredients are described per 100g of product!
+
+
+class Serving(typing.NamedTuple):
+    product_id: str  # reference to product name
+    factor: float  # multiplier for product ingredients
+    macros: Ingredients  # resulting amount of ingredients _after_ multiplier is applied!
+    kcal: float  # resulting amount of kcal _after_ multiplier is applied!
 
 
 def _ing(f, c, p) -> Ingredients:
@@ -30,6 +37,7 @@ PRODUCTS = [
     Product("gorgonzola", 346, _ing(29.2, 0.4, 20.4)),
     Product("leinsamen", 518, _ing(42, 3, 18)),
     Product("harzer", 125, _ing(0.5, 0.1, 30)),
+    Product("kokosoel", 886, _ing(99, 0, 0)),
 ]
 
 del _ing
@@ -46,13 +54,15 @@ CONVERSION = {
 
 
 def energy_from_ingredients(igs: Ingredients) -> float:
+    """Convert ingredients to energy using individual conversion rates
+    """
     fats = CONVERSION["fat_to_kcal"] * igs.fat
     carbs = CONVERSION["carb_to_kcal"] * igs.carb
     prots = CONVERSION["prot_to_kcal"] * igs.prot
     return fats + carbs + prots
 
 
-def serving(factor: float, prod: Product) -> Serving:
+def calc_serving_by_product_factor(factor: float, prod: Product) -> Serving:
     """Calculate a serving based on a factor of product
 
     Since products are stored as 100g, the factor specifies
@@ -70,18 +80,18 @@ def serving(factor: float, prod: Product) -> Serving:
     )
 
 
-def calc_serving_for_macro(macro_name:str, grams, prod: Product):
-    """Calculate a serving given a target amount carbs
+def calc_serving_for_macro(macro_name: str, grams, prod: Product):
+    """Calculate a serving given a target amount of a macro, e.g. fat
     """
     f = float(grams) / getattr(prod.ingredients, macro_name)
-    return serving(f, prod)
+    return calc_serving_by_product_factor(f, prod)
 
 
 def calc_serving_for_kcal(kcal, prod: Product):
     """Calculate a serving given a target amount of calories
     """
     f = float(kcal) / prod.kcal
-    return {'grams': 100.0 * f, 'serving': serving(f, prod)}
+    return {"grams": 100.0 * f, "serving": calc_serving_by_product_factor(f, prod)}
 
 
 def shorten_str(s, width):
@@ -89,22 +99,16 @@ def shorten_str(s, width):
     if len(s) <= width:
         return s
 
-    l2 = width // 2
-    s2 = s[0:ls]
-
-    l2 = s2 // 2
-    first = s2[0:l2-2]
-    last = s2[l2+2:]
-    return first + "..." + last
+    return s[0 : width - 3] + "..."
 
 
-def print_serving(s: Serving):
+def format_serving(s: Serving) -> str:
     macros = s.macros
     eng = energy_from_ingredients(macros)
 
-    w_name = 10
-    # name = textwrap.shorten(s.product_id, w_name, placeholder='..')
-    name = s.product_id
+    w_name = 15
+    name = shorten_str(s.product_id, w_name)
+
     s_fmt = f"{name:<{w_name}}:"
     s_fmt += f" {s.kcal:6.1f}"
     s_fmt += f" ({eng:6.1f}) | "
@@ -112,7 +116,7 @@ def print_serving(s: Serving):
     s_fmt += f" {macros.carb:5.1f}"
     s_fmt += f" {macros.prot:5.1f}"
 
-    print(s_fmt)
+    return s_fmt
 
 
 def summarize_servings(ls) -> dict:
@@ -128,54 +132,66 @@ def summarize_servings(ls) -> dict:
     ing = Ingredients(fats, carbs, prots)
     eng = energy_from_ingredients(ing)
 
-    percent_fat = CONVERSION['fat_to_kcal'] * fats / eng * 100.0
-    percent_carb = CONVERSION['carb_to_kcal'] * carbs / eng * 100.0
-    percent_prot = CONVERSION['prot_to_kcal'] * prots / eng * 100.0
+    percent_fat = CONVERSION["fat_to_kcal"] * fats / eng * 100.0
+    percent_carb = CONVERSION["carb_to_kcal"] * carbs / eng * 100.0
+    percent_prot = CONVERSION["prot_to_kcal"] * prots / eng * 100.0
 
-    return {"kcal": kcal, "macros": ing,
-        "macros_percent": [percent_fat, percent_carb, percent_prot]}
+    return {
+        "kcal": kcal,
+        "macros": ing,
+        "macros_percent": [percent_fat, percent_carb, percent_prot],
+    }
 
 
-def print_servings(ls):
-    s_fmt = 'PRODUCT'
-    s_fmt += '       kcal ( kcal2) | '
-    s_fmt += '   FAT  CARB  PROT'
-    print(s_fmt)
+def format_servings(ls) -> typing.List[str]:
+    lines = []
 
-    print('=' * 48)
+    s_fmt = "PRODUCT"
+    s_fmt += "            kcal ( kcal2) | "
+    s_fmt += "   FAT  CARB  PROT"
+    lines.append(s_fmt)
 
-    for s in ls:
-        print_serving(s)
-
-    print('-' * 48)
+    lines.append("=" * 53)
+    lines.extend(map(format_serving, ls))
+    lines.append("-" * 53)
 
     amount = summarize_servings(ls)
     macros = amount["macros"]
     percent = amount["macros_percent"]
     energy = energy_from_ingredients(macros)
 
-    s_fmt = " " * 10
+    s_fmt = " " * 15
     s_fmt += f'  {amount["kcal"]:6.1f}'
     s_fmt += f" ({energy:6.1f}) | "
     s_fmt += f" {macros.fat:5.1f}"
     s_fmt += f" {macros.carb:5.1f}"
     s_fmt += f" {macros.prot:5.1f}"
 
-    print(s_fmt)
+    lines.append(s_fmt)
 
-    s_fmt = " " * 24 + "[%] | "
+    s_fmt = " " * 23 + "[% kcal2] | "
     s_fmt += f"  {percent[0]:4.1f}  {percent[1]:4.1f}  {percent[2]:4.1f}"
-    print(s_fmt)
+    lines.append(s_fmt)
 
+    return lines
+
+
+MEALS_SATURDAY = [
+    calc_serving_by_product_factor(2.5, DATABASE["skyr"]),
+    calc_serving_by_product_factor(1.25, DATABASE["feta"]),
+    calc_serving_by_product_factor(0.05, DATABASE["leinsamen"]),
+    calc_serving_by_product_factor(1.74, DATABASE["ei"]),
+    calc_serving_by_product_factor(4.5, DATABASE["spinat"]),
+    calc_serving_by_product_factor(0.5, DATABASE["gorgonzola"]),
+    calc_serving_by_product_factor(1.0, DATABASE["harzer"]),
+]
 
 MEALS = [
-    serving(2.5, DATABASE["skyr"]),
-    serving(1.25, DATABASE["feta"]),
-    serving(0.05, DATABASE["leinsamen"]),
-    serving(1.74, DATABASE["ei"]),
-    serving(4.5, DATABASE["spinat"]),
-    serving(0.5, DATABASE["gorgonzola"]),
-    serving(1.0, DATABASE["harzer"]),
+    calc_serving_by_product_factor(2.0, DATABASE["skyr"]),
+    calc_serving_by_product_factor(1.25, DATABASE["feta"]),
+    calc_serving_by_product_factor(0.05, DATABASE["leinsamen"]),
+    calc_serving_by_product_factor(1.74, DATABASE["ei"]),
+    calc_serving_by_product_factor(0.07, DATABASE["kokosoel"]),
 ]
 
 
@@ -183,22 +199,24 @@ def main():
     # print(DATABASE)
 
     # print(MEALS[0])
-    print_servings(MEALS)
+    print("\n".join(format_servings(MEALS)))
 
-    print('-------------')
-    foo = calc_serving_for_macro('prot', 10, DATABASE['ei'])
-    print_serving(foo)
-    print('-------------')
-    bar = calc_serving_for_kcal(1000, DATABASE['ei'])
-    print(f'grams: {bar["grams"]:6.2f}')
-    print_serving(bar["serving"])
-    print('-------------')
-    buzz = serving(10 * 65 / 100, DATABASE['ei'])
-    # print_serving(buzz)
-    print_servings([buzz])
+    if False:
+        print("-------------")
+        foo = calc_serving_for_macro("prot", 10, DATABASE["ei"])
+        print(format_serving(foo))
+        print("-------------")
+        bar = calc_serving_for_kcal(1000, DATABASE["ei"])
+        print(f'grams: {bar["grams"]:6.2f}')
+        print(format_serving(bar["serving"]))
+        print("-------------")
+        buzz = calc_serving_by_product_factor(10 * 65 / 100, DATABASE["ei"])
+        # print_serving(buzz)
+        print(format_servings([buzz]))
 
-    # print(shorten_str('michael rath', 5))
-    # print('123456789')
+        print(shorten_str("michael rath", 8))
+        print("123456789")
+
 
 if __name__ == "__main__":
     main()
